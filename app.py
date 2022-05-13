e6ece3d88620f36884a09f3c794a2f8868792e02
Sreerag M <sreeragibtl@gmail.com>
2022-05-13 14:08:55 +0530

    Create flask app

---
+++ app.py
@@ -0,0 +1,14 @@
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route("/")
+def camera():
+
+    """html
+    
+    Deliver the html page for
+    camera.
+    """
+
+    return flask.render_template("html/camera.html")
